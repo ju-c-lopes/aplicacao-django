7159d95fd916dc3293e2b4f88d6adbfbbad333d2
Juliano <juliano.co.lopes@gmail.com>
2023-09-25 20:18:34 -0300

    corrigido botao menu mobile

--- gerenciaAula/static/js/responsive.js
+++ gerenciaAula/static/js/responsive.js
@@ -1,9 +1,7 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-}
+const btn = document.querySelector("#btn-nav");
+const menu = document.querySelector("#menu");
+btn.onclick = function() {
+  btn.classList.toggle('active')
+  menu.classList.contains("open") ? menu.classList.remove("open") : menu.classList.add("open");
+};
