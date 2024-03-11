82dc5e841649bcfab0a8280efa567626262d600a
Juliano <juliano.co.lopes@gmail.com>
2024-03-10 22:23:53 -0300

    tá dificil demais

--- projeto/settings/settings.py
+++ projeto/settings/settings.py
@@ -29,6 +29,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost', '*', 'pi3votorantim.pythonanywhere.com']
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'settings/db.sqlite3',
+    }
+}
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
