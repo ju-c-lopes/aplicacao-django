4b33792de83e82bbcd3f71a7b0d2c1e40de036da
Juliano <juliano.co.lopes@gmail.com>
2024-04-21 15:33:41 -0300

    arquivo dados js atualizado no estático

--- static/js/salvarDados.js
+++ static/js/salvarDados.js
@@ -1,10 +1,21 @@
 var buttonSaveCSV = document.querySelector(".btn-save-csv");
+var buttonSaveGraph = document.querySelector(".btn-save-grafico");
 
 buttonSaveCSV.onclick = () => {
     var csvContent = document.querySelector('#csvContent').value;
     var blob = new Blob([csvContent], {type: 'text/csv'});
-    var link = document.createElement('a');
+    let link = document.createElement('a');
     link.href = window.URL.createObjectURL(blob);
     link.download = 'dados.csv';
     link.click();
-}
+};
+
+buttonSaveGraph.onclick = () => {
+    console.log("tentando salvar")
+    var grafico = document.querySelector("#imagem-grafico");
+    var imageBase64 = grafico.src;
+    var link = document.createElement('a');
+    link.href = imageBase64;
+    link.download = 'grafico.png'
+    link.click();
+};
