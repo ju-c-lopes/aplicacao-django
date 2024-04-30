2d1530f3bcd6522608ec8365b47bdf2140dfe46a
Juliano <juliano.co.lopes@gmail.com>
2024-04-30 19:49:20 -0300

    resolvido estaticos

--- static/js/salvarDados.js
+++ static/js/salvarDados.js
@@ -1,21 +1,24 @@
-var buttonSaveCSV = document.querySelector(".btn-save-csv");
-var buttonSaveGraph = document.querySelector(".btn-save-grafico");
+var buttonSaveCSV = document.querySelectorAll(".btn-save-csv");
+var buttonSaveGraph = document.querySelectorAll(".btn-save-grafico");
 
-buttonSaveCSV.onclick = () => {
-    var csvContent = document.querySelector('#csvContent').value;
-    var blob = new Blob([csvContent], {type: 'text/csv'});
-    let link = document.createElement('a');
-    link.href = window.URL.createObjectURL(blob);
-    link.download = 'dados.csv';
-    link.click();
-};
+for (let i = 0; i < buttonSaveCSV.length; i++) {
+    buttonSaveCSV[i].onclick = () => {
+        var csvContent = document.querySelector(`#csvContent${i}`).value;
+        var blob = new Blob([csvContent], {type: 'text/csv'});
+        let link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.download = 'dados.csv';
+        link.click();
+    };
+}
 
-buttonSaveGraph.onclick = () => {
-    console.log("tentando salvar")
-    var grafico = document.querySelector("#imagem-grafico");
-    var imageBase64 = grafico.src;
-    var link = document.createElement('a');
-    link.href = imageBase64;
-    link.download = 'grafico.png'
-    link.click();
-};
+for (let i = 0; i < buttonSaveGraph.length; i++) {
+    buttonSaveGraph[i].onclick = () => {
+        var grafico = document.querySelector(`#imagem-grafico${i}`);
+        var imageBase64 = grafico.src;
+        var link = document.createElement('a');
+        link.href = imageBase64;
+        link.download = 'grafico.png'
+        link.click();
+    };
+}
