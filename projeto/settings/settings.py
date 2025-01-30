814e036b1922247493143d00061f02d914d36abf
Juliano <juliano.co.lopes@gmail.com>
2025-01-30 16:47:36 -0300

    alterando media_root

--- projeto/settings/settings.py
+++ projeto/settings/settings.py
@@ -161,9 +161,6 @@ if not DEBUG:
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = f"{AWS_S3_CUSTOM_DOMAIN}"
-
 STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
@@ -186,6 +183,9 @@ STORAGES = {
         },
     },
 }
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
